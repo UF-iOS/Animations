d85d8798266189b9d834decabdc267aa3f9634e9
Zachary Cowan <zacowan.dev@gmail.com>
2019-01-29 23:43:08 -0500

    Fixed launch animation.

--- ufios-spring2019-1-animations-dev/ViewController.swift
+++ ufios-spring2019-1-animations-dev/ViewController.swift
@@ -11,56 +11,42 @@ import Lottie
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var appleImageView: UIImageView!
     @IBOutlet weak var ufiosImageView: UIImageView!
     @IBOutlet weak var labelTop: UILabel!
     @IBOutlet weak var labelBottom: UILabel!
     @IBOutlet weak var continueButton: UIButton!
-    @IBOutlet weak var appleImageHorizontalConstraint: NSLayoutConstraint!
-    @IBOutlet weak var appleImageVerticalConstraint: NSLayoutConstraint!
+    @IBOutlet weak var ufiosCenterY: NSLayoutConstraint!
     
-    @IBOutlet weak var ufiosVerticalConstraint: NSLayoutConstraint!
     
-    let animationView = LOTAnimationView(name: "appleDevicesLottie")
+    var animationView: LOTAnimationView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .black
-     
-        appleImageView.image = appleImageView.image?.withRenderingMode(.alwaysTemplate)
-        appleImageView.tintColor = .white
         
-        ufiosImageView.alpha = 0
         labelTop.alpha = 0
         labelBottom.alpha = 0
         continueButton.alpha = 0
-        animationView.alpha = 0
         
-        UIView.animate(withDuration: 2.5, delay: 0.0, options: [.curveEaseOut], animations: {
-            self.view.backgroundColor = .white
-            self.appleImageView.tintColor = .black
+        // For some reason, need to include this to prevent a weird transition from the launch screen to this screen.
+        self.view.layoutSubviews()
+        
+        UIView.animate(withDuration: 2, delay: 1.0, options: [.curveEaseOut], animations: {
+            self.ufiosCenterY.constant = -200
+            self.view.layoutIfNeeded()
         }, completion: { (done) -> () in
-            UIView.animate(withDuration: 1, delay: 0.0, options: [.curveEaseOut], animations: {
-                self.appleImageVerticalConstraint.constant = -90
-                self.ufiosImageView.alpha = 1
-                self.ufiosVerticalConstraint.constant = 50
-                self.view.layoutIfNeeded()
+            UIView.animate(withDuration: 1, delay: 1.0, options: [.curveEaseOut], animations: {
+                self.ufiosImageView.alpha = 0
             }, completion: { (done) -> () in
-                UIView.animate(withDuration: 1, delay: 1.0, options: [.curveEaseOut], animations: {
-                    self.appleImageView.alpha = 0
-                    self.ufiosImageView.alpha = 0
+                self.animationViewSetup()
+                UIView.animate(withDuration: 1, delay: 0.5, options: [.curveEaseOut], animations: {
+                    self.labelTop.alpha = 1
+                    self.animationView!.alpha = 1
                 }, completion: { (done) -> () in
-                    self.animationViewSetup()
                     UIView.animate(withDuration: 1, delay: 0.5, options: [.curveEaseOut], animations: {
-                        self.labelTop.alpha = 1
-                        self.animationView.alpha = 1
+                        self.labelBottom.alpha = 1
                     }, completion: { (done) -> () in
-                        UIView.animate(withDuration: 1, delay: 0.5, options: [.curveEaseOut], animations: {
-                            self.labelBottom.alpha = 1
-                        }, completion: { (done) -> () in
-                            UIView.animate(withDuration: 1, delay: 1.0, options: [.curveEaseOut], animations: {
-                                self.continueButton.alpha = 1
-                            })
+                        UIView.animate(withDuration: 1, delay: 1.0, options: [.curveEaseOut], animations: {
+                            self.continueButton.alpha = 1
                         })
                     })
                 })
@@ -70,19 +56,23 @@ class ViewController: UIViewController {
     }
     
     func animationViewSetup() {
-        self.view.addSubview(self.animationView)
-        self.animationView.translatesAutoresizingMaskIntoConstraints = false
+        self.animationView = LOTAnimationView(name: "appleDevicesLottie")
         
-        self.animationView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 10).isActive = true
-        self.animationView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -10).isActive = true
-        self.animationView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
-        self.animationView.heightAnchor.constraint(equalTo: self.animationView.widthAnchor).isActive = true
+        self.animationView!.alpha = 0
         
-        self.animationView.autoReverseAnimation = true
+        self.view.addSubview(self.animationView!)
+        self.animationView!.translatesAutoresizingMaskIntoConstraints = false
         
-        self.animationView.loopAnimation = true
+        self.animationView!.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 10).isActive = true
+        self.animationView!.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -10).isActive = true
+        self.animationView!.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        self.animationView!.heightAnchor.constraint(equalTo: self.animationView!.widthAnchor).isActive = true
         
-        self.animationView.play()
+        self.animationView!.autoReverseAnimation = true
+        
+        self.animationView!.loopAnimation = true
+        
+        self.animationView!.play()
     }
 
 }
